8a96ead84436a0f76899408650bc64208a154eeb
Mark <markapuyanph@gmail.com>
2020-08-18 11:11:27 +0800

    [DEVELOP] Fix App Routing

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
 import React, { Suspense, useEffect } from 'react';
 import { Switch, Route, Redirect} from 'react-router-dom'
 import Layout from './hoc/AppLayout/AppLayout'
-import Spinner from './components/UI/Spinner/Spinner'
 import Auth from './containers/Auth/Auth'
-import { connect } from 'react-redux'
-import * as actions from 'store/actions/index'
-import './App.css';
-import PrivateRoute from 'hoc/PrivateRoute/PrivateRoute';
-
 import Cart from 'containers/Cart/Cart'
 import Detail from 'components/ItemsList/Item/Detail/Detail'
 import Profile from 'containers/Profile/Profile'
 import Result from 'containers/Result/Result'
 import Main from 'containers/Main/Main'
+import PrivateRoute from 'hoc/PrivateRoute/PrivateRoute'
+import { connect } from 'react-redux'
+import * as actions from 'store/actions/index'
+import './App.css';
 
 const App = props => {
-  const { onCheckAuth } = props;
+
+  const { onCheckAuth } = props
   useEffect(() => {
     onCheckAuth()
   }, [onCheckAuth])
 
-
-  let routes = (
+  const routes = (
       <Switch>
-        <PrivateRoute authed={props.isAuthenticated} path="/cart" component={Cart}/>
-        <PrivateRoute authed={props.isAuthenticated} path="/profile" component={Profile}/>
-        <Route path="/item-detail" component={Detail} />
+        <PrivateRoute path="/cart" authed={props.isAuthenticated} component={Cart} />
+        <PrivateRoute path="/auth" authed={!props.isAuthenticated} component={Auth} />
+        <PrivateRoute path="/profile" authed={props.isAuthenticated} component={Profile} />
+        <Route path="/item-detail"component={Detail}  />
         <Route path="/result" component={Result} />
-        {props.isAuthenticated && <Route path="/auth" component={Auth}/>}
         <Route exact path="/" component={Main} />
         <Redirect to="/"/>
-      </Switch>)
+      </Switch>
+  )
   return (
     <div className="App">
-        <Suspense fallback={<Spinner/>}>
-          <Layout>
-            {routes}
-           </Layout>
-        </Suspense>
+      <Layout>
+          {props.isLoading && routes}
+      </Layout>
     </div>
   );
 }
 
 const mapStateToProps = state => {
   return {
-    token: state.auth.token,
-    expirationDate: state.auth.expirationDate,
     isAuthenticated: state.auth.isAuthenticated,
+    isLoading: state.auth.isLoading
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onCheckAuth: () => dispatch(actions.checkAuthenticate())
+    onCheckAuth: (token, expiryDate) => dispatch(actions.checkAuthenticate())
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
